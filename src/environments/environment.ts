3f814b5c75809257acdffecddcfceddce164c580
relson816 <relson.batayola@gmail.com>
2018-02-05 23:00:53 +0800

    Added firestore to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+    apiKey: "AIzaSyAdc4ZE3nXrZKICv8NedYxpi7skKWqHDos",
+    authDomain: "fir-midtermexam.firebaseapp.com",
+    databaseURL: "https://fir-midtermexam.firebaseio.com",
+    projectId: "fir-midtermexam",
+    storageBucket: "fir-midtermexam.appspot.com",
+    messagingSenderId: "566861047546"
     }
 };
